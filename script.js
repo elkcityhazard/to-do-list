03fb5ae8b0280e541e0015c9cded34d40c1c9a80
elkcityhazard <41174540+elkcityhazard@users.noreply.github.com>
2020-04-06 18:33:02 -0400

    Drag and Drop To Do List - no frills
    localStorage, drag and drop functionality, clear function

---
+++ script.js
@@ -0,0 +1,93 @@
+const form = document.querySelector('form');
+const addTask = document.getElementById('task');
+var storedList = [];
+var counter = 0;
+var dragItem;
+
+ init();
+
+/****************
+**Functions
+*****************/
+function init() {
+
+
+  storedList = JSON.parse(localStorage.getItem("todo-list"));
+  if (storedList) {
+   storedList.forEach(function (item, index) {
+    const storedLI = document.createElement('li');
+    const storedText = document.createTextNode(/*index + '. ' + */item);
+    storedLI.id = 'taskID-' + counter;
+    storedLI.draggable = "true";
+    storedLI.classList = 'drag-me';
+    storedLI.setAttribute("ondragstart", "drag(event)");
+    storedLI.appendChild(storedText);
+    document.getElementById('list-items').appendChild(storedLI);
+    counter++;
+  })
+} else {
+  return storedList = [];
+  }
+}
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData('text', ev.target.id);
+  console.log(ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData('text');
+  ev.target.appendChild(document.getElementById(data));
+}
+
+/****************
+**Event Listeners
+*****************/
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   var dragItem = document.getElementsByClassName('drag-me');
+// })
+
+form.addEventListener('submit', function(e) {
+
+  //  1. prevent default
+  e.preventDefault();
+
+  //  2. check to see if input is empty
+  if (task.value === '') {
+    document.querySelector('.small').style.display="block";
+  } else {
+    //  2. create new element
+    var newLI = document.createElement('li');
+    newLI.classList = 'drag-me';
+    newLI.id = 'taskID-' + counter;
+    newLI.draggable = "true";
+    newLI.setAttribute("ondragstart", "drag(event)");
+    var taskText = document.createTextNode(addTask.value);
+    storedList.push(addTask.value);
+    //  3. give element things
+    newLI.appendChild(taskText);
+    //  4. grab where the element goes
+    var listContainer = document.getElementById('list-items');
+    //  5. append to end of new element
+    listContainer.appendChild(newLI);
+
+    //  6. Reset Input, Increase Counter For ID
+    addTask.value = '';
+    document.querySelector('.small').style.display="none";
+    localStorage.setItem('todo-list', JSON.stringify(storedList));
+    counter ++;
+  }
+});
+
+document.getElementById('reset-button').addEventListener('click', function(e) {
+  e.preventDefault();
+  localStorage.clear();
+  location.reload();
+  addTask.value = '';
+});
